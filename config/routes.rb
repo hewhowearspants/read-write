b3062dd0f5bc1b6aa3eca3fc8989566b88a868be
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-13 12:25:36 -0400

    add routes for User and Session

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+  get "/profile" => "users#profile"
+
+  resources :users
+  
 end
