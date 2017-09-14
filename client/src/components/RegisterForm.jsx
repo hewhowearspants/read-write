eb57aed5f0623b6962d4a67b08812ded6a9e0d62
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-14 16:54:47 -0400

    create RegisterForm component

---
+++ client/src/components/RegisterForm.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class RegisterForm extends Component {
+  render() {
+    return (
+      <div className="registerform">
+        <form onSubmit={this.props.handleRegisterSubmit}>
+          <input
+            type="text"
+            name="registerUsername"
+            value={this.props.registerUsername}
+            placeholder="username"
+            onChange={this.props.handleInputChange}
+          />
+          <input
+            type="password"
+            name="registerPassword"
+            value={this.props.registerPassword}
+            placeholder="password"
+            onChange={this.props.handleInputChange}
+          />
+          <input
+            type="email"
+            name="registerEmail"
+            value={this.props.registerEmail}
+            placeholder="email"
+            onChange={this.props.handleInputChange}
+          />
+          <input
+            type="text"
+            name="registerName"
+            value={this.props.registerName}
+            placeholder="name"
+            onChange={this.props.handleInputChange}
+          />
+          <input type="submit" value="Register" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default RegisterForm;
