e389fa1eae2d5ccc71ad40bae7d6eb144b1522a9
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-14 09:14:10 -0400

    add relational info for Chapter model

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,2 +1,4 @@
 class Chapter < ApplicationRecord
+  belongs_to :project
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :users
   resources :books
   resources :projects
+  resources :chapters
 end
