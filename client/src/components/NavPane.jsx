7cc96c7747b88c3457fe694840e5855aef2f9c58
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-14 21:43:01 -0400

    add links to BookList and ProjectList to NavPane

--- client/src/components/NavPane.jsx
+++ client/src/components/NavPane.jsx
@@ -8,7 +8,18 @@ class NavPane extends Component {
     return (
       <nav>
         <ul>
-          {!Auth.isUserAuthenticated() ? (<li><Link to="/login">Login</Link></li>) : (<li className="logout" onClick={this.props.logoutUser}>Logout</li>)}
+          {!Auth.isUserAuthenticated() ? (
+            <ul>
+              <li><Link to="/login">Login</Link></li>
+            </ul>
+          ) : (
+            <ul>
+              <li><Link to="/profile">Dash</Link></li>
+              <li><Link to="/books">Read</Link></li>
+              <li><Link to="/projects">Write</Link></li>
+              <li className="logout" onClick={this.props.logoutUser}>Logout</li>
+            </ul>
+          )}
         </ul>
       </nav>
     )
