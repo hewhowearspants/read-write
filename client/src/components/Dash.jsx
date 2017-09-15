42a73480efab4e469be828719e0398342b5c510b
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-14 20:20:38 -0400

    implement user Dashboard (Dash) component

---
+++ client/src/components/Dash.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+import axios from 'axios';
+
+import Auth from '../modules/Auth';
+
+class Dash extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userBooks: null,
+      userProjects: null,
+      username: null,
+      userEmail: null,
+      name: null,
+    }
+  }
+
+  componentWillMount() {
+    axios.get('/profile', {
+      headers: {
+        'Authorization': `Token ${Auth.getToken()}`,
+        token: Auth.getToken(),
+      }
+    })
+    .then((res) => {
+      this.setState({
+        userBooks: res.data.books,
+        userProjects: res.data.projects,
+        username: res.data.user.username,
+        userEmail: res.data.user.email,
+        name: res.data.user.name
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  render() {
+    return (
+      <div className='dashboard'>
+        {this.state.name ? (<h2>Hi there, {this.state.name}</h2>) : (<h2>Loading...</h2>)}
+        {this.state.userBooks ? (<p>You have {this.state.userBooks.length} books in your list</p>) : ''}
+        {this.state.userProjects ? (<p>You have {this.state.userProjects.length} projects in the works</p>) : ''}
+      </div>
+    )
+  }
+}
+
+export default Dash
