eec26f2521efec22bff42db2bb597094b1fe7909
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-14 21:41:19 -0400

    create BookList component

---
+++ client/src/components/BookList.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Auth from '../modules/Auth';
+import BookSingle from './BookSingle';
+
+
+class BookList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      bookData: null,
+      bookDataLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/books', {
+      headers: {
+        'Authorization': `Token ${Auth.getToken()}`,
+        token: Auth.getToken(),
+      }
+    }).then((res) => {
+      this.setState({
+        bookData: res.data.books,
+        bookDataLoaded: true,
+      })
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  showBooks() {
+    return this.state.bookData.map((book) => {
+      return (
+        <BookSingle type="list" book={book} key={book.id} />
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className='booklist'>
+        {(this.state.bookDataLoaded ? this.showBooks() : <p>Loading...</p>)}
+      </div>
+    )
+  }
+}
+
+export default BookList;
