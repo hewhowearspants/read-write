40be30a2a82e5877c7127974557ee506129cb781
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-15 15:48:07 -0400

    update BookSingle component to account for inline edit form fields

--- client/src/components/BookSingle.jsx
+++ client/src/components/BookSingle.jsx
@@ -3,10 +3,29 @@ import React from 'react';
 const BookSingle = (props) => {
   return (
     <div className={`book-${props.type}`}>
-      <div className="book-single-top">
-        <p>{props.book.title}</p>
-        <p>{props.book.author}</p>
-      </div>
+      {props.bookToEdit !== props.book.id ? (
+        <div className="book-single-top">
+          <p>{props.book.title}</p>
+          <p>{props.book.author}</p>
+        </div>
+      ) : (
+        <div className="book-single-top">
+          <input
+            type="text"
+            name="bookTitle"
+            value={props.bookTitle}
+            placeholder="title"
+            onChange={props.handleInputChange}
+          />
+          <input
+            type="text"
+            name="bookAuthor"
+            value={props.bookAuthor}
+            placeholder="author"
+            onChange={props.handleInputChange}
+          />
+        </div>
+      )}
       <div className="book-single-bottom">
         <div className="book-show-info" onClick={() => props.setBookToShow(props.book.id)}>
           {props.bookToShow === props.book.id ? <p>-</p> : <p>+</p>}
