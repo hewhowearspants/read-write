eba07288af4baf7c5a9afb6387cb8762bea237cd
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-15 15:49:13 -0400

    update Book component to account for inline edit form fields

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -5,16 +5,68 @@ import React from 'react';
 const Book = (props) => {
   return (
     <div className="book">
-      <div className="book-description">
-        <p>{props.book.description}</p>
-      </div>
-      <div className="book-middle">
-        <p>Year: {props.book.year}</p>
-        <p>Genre: {props.book.genre}</p>
-      </div>
-      <div className="book-bottom">
-        <div className="links">
+      {props.bookToEdit !== props.book.id ? (
+        <div className="book-description">
+          <p>{props.book.description}</p>
         </div>
+      ) : (
+        <div className="book-description">
+          <input
+            type="text"
+            name="bookDescription"
+            value={props.bookDescription}
+            placeholder="description"
+            onChange={props.handleInputChange}
+          />
+        </div>
+      )}
+      {props.bookToEdit === props.book.id ? (
+        <div className="book-middle">
+          <input
+            type="text"
+            name="bookGenre"
+            value={props.bookGenre}
+            placeholder="genre"
+            onChange={props.handleInputChange}
+          />
+          <input
+            type="number"
+            name="bookYear"
+            value={props.bookYear}
+            placeholder="year"
+            onChange={props.handleInputChange}
+          />
+          <input
+            type="text"
+            name="bookImage"
+            value={props.bookImage}
+            placeholder="cover image url"
+            onChange={props.handleInputChange}
+          />
+        </div>
+      ) : (
+        <div className="book-middle">
+          <p>Year: {props.book.year}</p>
+          <p>Genre: {props.book.genre}</p>
+        </div>
+      )}
+      <div className="book-bottom">
+        {props.bookToEdit !== props.book.id ? (
+          <div className="links">
+            <p>
+              <span onClick={() => props.setBookToEdit(props.book.id)}>Edit</span>
+            </p>
+          </div>
+        ) : (
+          <div className="links">
+            {/* <form onSubmit={() => props.handleBookEditSubmit()}>
+              <input type="submit" value="Submit" />
+            </form> */}
+            <span onClick={() => props.handleBookEditSubmit()}>Submit</span>
+            <span onClick={() => props.setBookToEdit(null)}>Cancel</span>
+          </div>
+      )}
+          
       </div>
     </div>
   )
