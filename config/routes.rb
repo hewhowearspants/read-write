4968206f860684da0f6f32a70e0796df43ca6764
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-16 19:29:26 -0400

    add route for book api search

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post "/login" => "sessions#create"
   delete "/logout" => "sessions#destroy"
   get "/profile" => "users#profile"
+  put "/books/search" => "books#search"
 
   resources :users
   resources :books
