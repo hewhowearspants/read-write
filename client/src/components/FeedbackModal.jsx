68c0ee5fad9d11c1b039c54c48c902f151c18c24
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-18 13:44:43 -0400

    add labels for user feedback ratings

--- client/src/components/FeedbackModal.jsx
+++ client/src/components/FeedbackModal.jsx
@@ -13,15 +13,15 @@ class FeedbackModal extends Component {
           <form>
             <div className='rating'>
               <input onChange={this.props.handleInputChange} className="star star-5" id="star-5" type="radio" name="bookRating" value='5'/>
-              {/* <label className="star star-5" for="star-5"></label> */}
+              <label className="star star-5" for="star-5">5</label>
               <input onChange={this.props.handleInputChange} className="star star-4" id="star-4" type="radio" name="bookRating" value='4'/>
-              {/* <label className="star star-4" for="star-4"></label> */}
+              <label className="star star-4" for="star-4">4</label>
               <input onChange={this.props.handleInputChange} className="star star-3" id="star-3" type="radio" name="bookRating" value='3'/>
-              {/* <label className="star star-3" for="star-3"></label> */}
+              <label className="star star-3" for="star-3">3</label>
               <input onChange={this.props.handleInputChange} className="star star-2" id="star-2" type="radio" name="bookRating" value='2'/>
-              {/* <label className="star star-2" for="star-2"></label> */}
+              <label className="star star-2" for="star-2">2</label> 
               <input onChange={this.props.handleInputChange} className="star star-1" id="star-1" type="radio" name="bookRating" value='1'/>
-              {/* <label className="star star-1" for="star-1"></label> */}
+              <label className="star star-1" for="star-1">1</label> 
             </div>
             <textarea onChange={this.props.handleInputChange} name='bookComment' type='text' placeholder='Well...'></textarea>
           </form>
