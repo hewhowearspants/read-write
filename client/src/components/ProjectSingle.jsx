bc37631c7af1c021c0518b2b06600dc4e85480e0
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-19 14:03:00 -0400

    add class names to ProjectSingle component for CSS purposes

--- client/src/components/ProjectSingle.jsx
+++ client/src/components/ProjectSingle.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 
 const ProjectSingle = (props) => {
   return (
-    <div className={`project-${props.type}`}>
+    <div className={`project-single`}>
       <div className="project-single-top">
-        <p>{props.project.title}</p>
-        <p>{props.project.subtitle}</p>
+        <p className='project-title'>{props.project.title}</p>
+        <p className='project-subtitle'>{props.project.subtitle}</p>
       </div>
       <div className="project-single-bottom">
         <Link to={`/projects/${props.project.id}`}>Open</Link>
