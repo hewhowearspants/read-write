b6c24cd67e9e17444934a8abccd2f4160f8a35d5
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-19 19:28:30 -0400

    add tooltips to NewCharLocForm buttons

--- client/src/components/NewCharLocForm.jsx
+++ client/src/components/NewCharLocForm.jsx
@@ -19,8 +19,8 @@ const NewCharLocForm = (props) => {
           onChange={props.handleInputChange}
         />
         <div className='buttons'>
-          <button className='submit-button' type="submit"><i className="fa fa-share" aria-hidden="true"></i></button>
-          <button className='cancel-button' onClick={props.toggleCreateNew}><i className="fa fa-times" aria-hidden="true"></i></button>
+          <button className='submit-button' type="submit" title="Submit"><i className="fa fa-share" aria-hidden="true"></i></button>
+          <button className='cancel-button' onClick={props.toggleCreateNew} title="Cancel"><i className="fa fa-times" aria-hidden="true"></i></button>
         </div>
       </form>
     </div>
