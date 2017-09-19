78b883ce039287a3266bc2e7a5ca42ab8d0f9ac9
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-19 13:55:53 -0400

    change Chapter show component to DANGEROUSLY SET INNER HTML to render Quill output nicely...and dangerously

--- client/src/components/Chapter.jsx
+++ client/src/components/Chapter.jsx
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class Chapter extends Component {
-  constructor() {
-    super();
-  }
-  
-  componentDidMount() {
-    //axios(`/projects/${this.props.projectData.id}/chapters/${this.props.chapter.id}`)
-  }
-
   render() {
     return (
       <div className='chapter'>
         <p><span onClick={() => this.props.setChapterToShow(null)}>Chapters</span> > Chapter {this.props.chapter.chapter_number}: {this.props.chapter.title}</p> 
-        <p>{this.props.chapter.content}</p> 
+        <div dangerouslySetInnerHTML={{ __html: this.props.chapter.content}} /> 
+        <div className='buttons'>
+          <button onClick={() => this.props.setChapterToEdit(this.props.chapter.id)}>Edit</button>
+          <button onClick={() => this.props.deleteChapter(this.props.chapter.id)}>Delete</button>
+        </div>
       </div>
     )
   }
