aa15a751466df5d69624f13b680a5a040f35c085
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-19 19:25:06 -0400

    add class names and FontAwesome to Chapter component buttons

--- client/src/components/Chapter.jsx
+++ client/src/components/Chapter.jsx
@@ -7,8 +7,8 @@ class Chapter extends Component {
         <p className='chapter-header'><span onClick={() => this.props.setChapterToShow(null)}>Chapters</span> > Chapter {this.props.chapter.chapter_number}: {this.props.chapter.title}</p> 
         <div className='chapter-content' dangerouslySetInnerHTML={{ __html: this.props.chapter.content}} /> 
         <div className='buttons'>
-          <button onClick={() => this.props.setChapterToEdit(this.props.chapter.id)}>Edit</button>
-          <button onClick={() => this.props.deleteChapter(this.props.chapter.id)}>Delete</button>
+          <button className='edit-button' onClick={() => this.props.setChapterToEdit(this.props.chapter.id)}><i className="fa fa-pencil-square-o" aria-hidden="true"></i></button>
+          <button className='delete-button' onClick={() => this.props.deleteChapter(this.props.chapter.id)}><i className="fa fa-trash" aria-hidden="true"></i></button>
         </div>
       </div>
     )
