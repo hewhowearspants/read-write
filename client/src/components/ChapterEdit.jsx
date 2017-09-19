09ac0df9088ca6fcd78b5162806b0b56eb5b7afa
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-19 13:47:37 -0400

    fill in ChapterEdit component (using ReactQuill!)

---
+++ client/src/components/ChapterEdit.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import ReactQuill from 'react-quill';
+
+class ChapterEdit extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      text: '',
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(value) {
+    this.setState({
+      text: value,
+    })
+  }
+
+  render() {
+    return (
+      <div className='chapter-edit'>
+        <p><span onClick={() => this.props.setChapterToShow(null)}>Chapters</span> > 
+           Chapter <input type='text' name='chapterNumber' value={this.props.chapterNumber} placeholder='#' onChange={this.props.handleInputChange}/>
+           : <input type='text' name='chapterTitle' value={this.props.chapterTitle} placeholder='title' onChange={this.props.handleInputChange}/>
+        </p>
+        <ReactQuill value={this.props.chapterContent} onChange={this.props.handleContentChange} />
+        <div className='buttons'>
+          <button onClick={this.props.handleChapterEditSubmit}>Save</button>
+          <button onClick={() => this.props.setChapterToEdit(null)}>Back</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ChapterEdit;
