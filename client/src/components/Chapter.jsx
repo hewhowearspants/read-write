4654fc54459b6df72a74f41fd9d465fabc3d8d82
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-19 15:46:52 -0400

    add class names to Chapter component

--- client/src/components/Chapter.jsx
+++ client/src/components/Chapter.jsx
@@ -4,8 +4,8 @@ class Chapter extends Component {
   render() {
     return (
       <div className='chapter'>
-        <p><span onClick={() => this.props.setChapterToShow(null)}>Chapters</span> > Chapter {this.props.chapter.chapter_number}: {this.props.chapter.title}</p> 
-        <div dangerouslySetInnerHTML={{ __html: this.props.chapter.content}} /> 
+        <p className='chapter-header'><span onClick={() => this.props.setChapterToShow(null)}>Chapters</span> > Chapter {this.props.chapter.chapter_number}: {this.props.chapter.title}</p> 
+        <div className='chapter-content' dangerouslySetInnerHTML={{ __html: this.props.chapter.content}} /> 
         <div className='buttons'>
           <button onClick={() => this.props.setChapterToEdit(this.props.chapter.id)}>Edit</button>
           <button onClick={() => this.props.deleteChapter(this.props.chapter.id)}>Delete</button>
