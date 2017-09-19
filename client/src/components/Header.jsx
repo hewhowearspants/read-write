3ba77ec98bcd366502e856636a980e2848c593ed
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-19 13:45:10 -0400

    add "selected" class to navpane button when nav pane opens

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ class Header extends Component {
     return (
       <header>
         <h1>Read/Write</h1>
-        <p onClick={this.props.toggleNavPane}>NavPane</p>
+        <p onClick={this.props.toggleNavPane} className={this.props.showNavPane ? "selected" : ""}><i className="fa fa-bars" aria-hidden="true"></i></p>
       </header>
     )
   }
