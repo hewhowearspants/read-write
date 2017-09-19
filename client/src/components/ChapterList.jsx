f880a2c9397ae87731cf1dd7b034364142220b93
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-19 13:50:39 -0400

    create and bind ChapterList CRUD methods

--- client/src/components/ChapterList.jsx
+++ client/src/components/ChapterList.jsx
@@ -15,9 +15,22 @@ class ChapterList extends Component {
       chapterData: null,
       chapterDataLoaded: false,
       chapterToShow: null,
+      chapterToEdit: null,
+      creatingChapter: false,
+      //form fields
+      chapterNumber: '',
+      chapterTitle: '',
+      chapterContent: '',
     }
     
     this.setChapterToShow = this.setChapterToShow.bind(this);
+    this.setChapterToEdit = this.setChapterToEdit.bind(this);
+    this.toggleCreateChapter = this.toggleCreateChapter.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleContentChange = this.handleContentChange.bind(this);
+    this.handleChapterSubmit = this.handleChapterSubmit.bind(this);
+    this.handleChapterEditSubmit = this.handleChapterEditSubmit.bind(this);
+    this.deleteChapter = this.deleteChapter.bind(this);
   }
 
   componentDidMount() {
@@ -37,12 +50,145 @@ class ChapterList extends Component {
     });
   }
 
+  toggleCreateChapter() {
+    this.setState((prevState) => {
+      return {
+        creatingChapter: !prevState.creatingChapter,
+        chapterNumber: '',
+        chapterTitle: '',
+        chapterContent: '',
+      }
+    })
+  }
+
   setChapterToShow(id) {
     const chapterData = this.state.chapterData.filter((chapter) => {
       return chapter.id === id;
     })[0];
     this.setState({
       chapterToShow: chapterData,
+      chapterToEdit: null,
+    })
+  }
+
+  setChapterToEdit(id) {
+    const chapterData = this.state.chapterData.filter((chapter) => {
+      return chapter.id === id;
+    })[0];
+    if (chapterData) {
+      this.setState({
+        chapterToEdit: chapterData,
+        chapterNumber: chapterData.chapter_number,
+        chapterTitle: chapterData.title,
+        chapterContent: chapterData.content === null ? '' : chapterData.content,
+      })
+    } else {
+      this.setState({
+        chapterToEdit: null,
+        chapterNumber: '',
+        chapterTitle: '',
+        chapterContent: '',
+      })
+    }
+  }
+
+  handleInputChange(event) {
+    console.log(event.target);
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleContentChange(value) {
+    this.setState({
+      chapterContent: value,
+    })
+  }
+
+  handleChapterSubmit(event) {
+    event.preventDefault();
+    axios(`/projects/${this.props.projectData.id}/chapters`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Token ${Auth.getToken()}`,
+        token: Auth.getToken(),
+      },
+      data: {
+        chapter: {
+          chapter_number: this.state.chapterNumber,
+          title: this.state.chapterTitle,
+        }
+      }
+    }).then((res) => {
+      console.log(res);
+      this.setState((prevState) => {
+        return {
+          chapterData: prevState.chapterData.concat(res.data.chapter),
+          chapterNumber: '',
+          chapterTitle: '',
+          creatingChapter: false,
+        }
+      })
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  handleChapterEditSubmit() {
+    axios(`/projects/${this.props.projectData.id}/chapters/${this.state.chapterToEdit.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Token ${Auth.getToken()}`,
+        token: Auth.getToken(),
+      },
+      data: {
+        chapter: {
+          chapter_number: this.state.chapterNumber,
+          title: this.state.chapterTitle,
+          content: this.state.chapterContent,
+        }
+      }
+    }).then((res) => {
+      console.log(res);
+      const newChapterData = [...this.state.chapterData];
+      newChapterData.forEach((chapter, index, array) => {
+        if (chapter.id === res.data.chapter.id) {
+          array[index] = res.data.chapter;
+        }
+      });
+      this.setState({
+        chapterData: newChapterData,
+        chapterToShow: res.data.chapter,
+      })
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+  
+  deleteChapter(id) {
+    axios(`/projects/${this.props.projectData.id}/chapters/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Token ${Auth.getToken()}`,
+        token: Auth.getToken(),
+      }
+    }).then((res) => {
+      console.log(res);
+      const newChapterData = [...this.state.chapterData];
+      newChapterData.forEach((chapter, index, array) => {
+        if (chapter.id === id) {
+          array.splice(index, 1);
+        }
+      });
+      this.setState({
+        chapterData: newChapterData,
+        chapterToShow: null,
+        chapterToEdit: null,
+      });
+    }).catch((err) => {
+      console.log(err);
     })
   }
 
